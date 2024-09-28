44e2b47b60898eb9d033de3ce1ee7a6b133be33e
EC2 Default User <ec2-user@ip-172-31-8-165.ap-northeast-1.compute.internal>
2024-09-28 05:56:47 +0000

    [finish] assignment4

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,10 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
-  has_many :relationship, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :followers, through: :relationship, source: :follower
-
-  has_many :relationship_reverse, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :followings, through: :relationship_reverse, source: :followed
-
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :relationships_reverse, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :relationships_reverse, source: :follower
 
 
   has_one_attached :profile_image
@@ -25,12 +23,12 @@ class User < ApplicationRecord
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
 
-  def follow(user)
-    relationships.create(followerd_id: user_id)
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
   end
 
-  def unfollow(user)
-    relationships.find_by(followed_id: user.id).destroy
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
   end
 
   def following?(user)
